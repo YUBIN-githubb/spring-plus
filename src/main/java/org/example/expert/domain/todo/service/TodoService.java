4c907e7cc49cef7b165ad094e4684f5a58b75d8a
Moon YuBin <yubin5744@gmail.com>
2025-03-13 12:05:10 +0900

    1번 과제 : @Transactional(readOnly = true) 공부

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -19,23 +19,34 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+
+// @Transactional에 (readOnly = true)가 붙으면 읽기전용 트랜잭션으로 사용하겠다는 의미이다.
+// 따라서 SELECT 쿼리만 실행 가능하고 JPA가 따로 변경감지를 하지 않는다.
+// (readOnly = true)는 데이터 변경이 없는 메서드에서 실수로 save() 같은 DB를 변경하는 쿼리를 실행하는 걸
+// 방지할 때 사용하도록 하자.
 public class TodoService {
 
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+    @Transactional
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
+
+        // 유저 정보 받아오기
         User user = User.fromAuthUser(authUser);
 
+        // 날씨 정보 받아오기
         String weather = weatherClient.getTodayWeather();
 
+        // 새로운 Todo 생성
         Todo newTodo = new Todo(
                 todoSaveRequest.getTitle(),
                 todoSaveRequest.getContents(),
                 weather,
                 user
         );
+
+        // DB에 저장
         Todo savedTodo = todoRepository.save(newTodo);
 
         return new TodoSaveResponse(
@@ -47,6 +58,7 @@ public class TodoService {
         );
     }
 
+    @Transactional(readOnly = true)
     public Page<TodoResponse> getTodos(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
@@ -63,6 +75,7 @@ public class TodoService {
         ));
     }
 
+    @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
